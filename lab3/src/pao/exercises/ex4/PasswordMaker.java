b8d57a7fb4a01c57de4c2ab5232ad42aa0cd6180
DenisaCantu14 <62349123+DenisaCantu14@users.noreply.github.com>
2021-03-11 20:00:35 +0200

    added more details

--- lab3/src/pao/exercises/ex4/PasswordMaker.java
+++ lab3/src/pao/exercises/ex4/PasswordMaker.java
@@ -19,24 +19,26 @@ public class PasswordMaker
         Random r = new Random();
         String str = "";
         for (int i = 0; i < length; i++)
+        {
             str += (char) (48 + r.nextInt(47));
+        }
         return str;
     }
 
     public static String getPassword(String s)
     {
         if (instance == null)
+        {
             instance = new PasswordMaker(s);
+        }
 
         String password = "";
         //un sir random de lungime magic_number
         password += getMagicString(MAGIC_NUMBER);
 
         Random r = new Random();
-        for (int i = 0; i < 10; i++)
-            //10 caractere din magic_string
-            password += MAGIC_STRING.charAt(r.nextInt(20));
-
+        //10 caractere din magic_string
+        password += MAGIC_STRING.substring(0,10);
         //lungimea atributului name ca string
         password += Integer.toString(instance.name.length());
 
